7493d07a51b023a720d103990b8926d1d147362f
abembarka <abembarka@E1013104>
2019-05-02 14:19:54 +0200

    Init Moq librery

--- App1.Test/OrderManagerTest.cs
+++ App1.Test/OrderManagerTest.cs
@@ -3,14 +3,22 @@ using Xunit;
 
 namespace App1.Test
 {
+    using Models;
+    using Moq;
+
     public class OrderManagerTest
     {
         [Fact]
         public void Test1()
         {
-            var orderManager = new OrderManager();
-            orderManager.Submit(Product.Keyboard, "Bembarka abdekhalel", "05/20");
-            Assert.ThrowsAny<Exception>(()=>orderManager.Submit(Product.Keyboard, "Bembarka abdekhalel", "05/20"));
+
+            var productStockRepositoryMoq = new Mock<IProductStockRepository>();
+            productStockRepositoryMoq.Setup(m => m.IsInStock(It.IsAny<Product>())).Returns(false);
+            var paymentProcessorMoq = new Mock<IPaymentProcessor>();
+            var shippingProcessor = new Mock<IShippingProcessor>();
+
+            var orderManager = new OrderManager(shippingProcessor.Object,productStockRepositoryMoq.Object,paymentProcessorMoq.Object);
+            Assert.ThrowsAny<Exception>(() => orderManager.Submit(Product.Keyboard, "Bembarka abdekhalel", "05/20"));
         }
     }
 }
